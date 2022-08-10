b62de3cf3c97696d0ef3a01d32d6fdaf23192aa2
mengjingzhimo <luoboming@huawei.com>
2022-08-10 16:57:52 +0800

    Signed-off-by: mengjingzhimo <luoboming@huawei.com>
    Changes to be committed:
    	modified:   frameworks/resmgr/BUILD.gn
    	new file:   frameworks/resmgr/include/utils/psue_manager.h
    	deleted:    frameworks/resmgr/src/utils/psue_manager
    	modified:   frameworks/resmgr/src/utils/psue_manager.cpp

--- frameworks/resmgr/src/utils/psue_manager.cpp
+++ frameworks/resmgr/src/utils/psue_manager.cpp
@@ -1,19 +1,91 @@
-本地化级别控制，默认为3
-1、前后加中括号
-2、字母替换
-3、长度增加
+/*
+ * Copyright (c) 2021-2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-返回处理好的字符串 伪本地化（字符串） 
-{
-	if (级别 >= 2) {
-		字符替换
-	}
-	if (级别 == 3) {
-		加长度
-	}
-	if（级别>= 1） {
-		加中括号
-	}
+#include "utils/psue_manager.h"
+
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <unzip.h>
+#include <vector>
+
+#include "hilog_wrapper.h"
+#inlcude "map"
+#include "utils/errors.h"
+
+namespace OHOS {
+namespace Global {
+namespace Resource {
+using namespace std;
+
+namespace {
+const float DEFAULT_EXTEND_RATIO = 0.3f;
+const int DEFAULT_PSUE_LEVEL = 3;
+
+stuct ExtendRatioTable {
+    int32_t count;
+	float ratio;
+}
+const ExtendRatioTable EXTERN_RATIO_TABLE[] = {
+    {10, 2.0f},
+    {20, 1.0f},
+    {30, 0.8f},
+    {50, 0.6f},
+    {70, 0.4f},
+};
+
+const map<wchar_t, wchar_t> REPLACE_TABLE {
+    {L'a', L'a'},
+    {L'A', L'a'},
+    {L'c', L'a'},
+    {L'C', L'a'},
+    {L'i', L'a'},
+    {L'I', L'a'},
+    {L'o', L'a'},
+    {L'O', L'a'},
+    {L'u', L'a'},
+    {L'U', L'a'},
+    {L'y', L'a'},
+    {L'Y', L'a'},
+    {L'z', L'a'},
+    {L'Z', L'a'},
+    {L's', L'a'},
+    {L'S', L'a'},
+};
 }
 
-伪本地化级别设置（1~3）
+PsueManager::PsueManager()
+{
+}
+
+PsueManager::~PsueManager()
+{
+}
+
+/**
+PsuedoTranslation level default value is 3
+1. Enclosed in brackets
+2. Letter replacement
+3. Lengthen string
+*/
+
+std::string PsueManager::Convert(const std::string &src, std::string &dest)
+{
+    return src;
+}
+
+} // namespace Resource
+} // Global
+} // OHOS
