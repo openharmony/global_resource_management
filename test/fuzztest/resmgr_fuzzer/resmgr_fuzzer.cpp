cea1d3ec93e21fa5e9159cf246045a52d6b0c332
fangyunzhong <fangyunzhong2@huawei.com>
2024-03-06 17:51:01 +0800

    新增fuzztest测试用例
    Signed-off-by: fangyunzhong <fangyunzhong2@huawei.com>

--- test/fuzztest/resmgr_fuzzer/resmgr_fuzzer.cpp
+++ test/fuzztest/resmgr_fuzzer/resmgr_fuzzer.cpp
@@ -26,13 +26,69 @@ using namespace std;
 using namespace OHOS::Global::Resource;
 
 namespace OHOS {
+
+    constexpr int SINGULAR_NUM = 1;
+    constexpr int PLURAL_NUM = 2;
+
+    bool AddResourceFuzzTest(const char* data, size_t size, ResourceManager *rm)
+    {
+        bool result = false;
+        if (size > 0) {
+            std::string testName(data, size);
+            result = rm->AddResource(testName.c_str());
+        }
+        return result;
+    }
+
+    bool RemoveResourceFuzzTest(const char* data, size_t size, ResourceManager *rm)
+    {
+        bool result = false;
+        std::vector<std::string> overlayPaths;
+        if (size > 0) {
+            std::string testName(data, size);
+            result = rm->RemoveResource(testName.c_str(), overlayPaths);
+        }
+        return result;
+    }
+
+    bool AddAppOverlayFuzzTest(const char* data, size_t size, ResourceManager *rm)
+    {
+        bool result = false;
+        if (size > 0) {
+            std::string testName(data, size);
+            result = rm->AddAppOverlay(testName.c_str());
+        }
+        return result;
+    }
+
+    bool RemoveAppOverlayFuzzTest(const char* data, size_t size, ResourceManager *rm)
+    {
+        bool result = false;
+        if (size > 0) {
+            std::string testName(data, size);
+            result = rm->RemoveAppOverlay(testName.c_str());
+        }
+        return result;
+    }
+
     bool GetStringByIdFuzzTest(const char* data, size_t size, ResourceManager *rm)
     {
         bool result = false;
         if (size > 0) {
-            uint32_t testId = reinterpret_cast<uint32_t>(data);
+            uint32_t testId = static_cast<uint32_t>(atoi(data));
             std::string extraInfo = "";
             result = rm->GetStringById(testId, extraInfo);
+        }
+        return result;
+    }
+
+    bool GetStringByNameFuzzTest(const char* data, size_t size, ResourceManager *rm)
+    {
+        bool result = false;
+        if (size > 0) {
+            std::string testName(data, size);
+            std::string extraInfo = "";
+            result = rm->GetStringByName(testName.c_str(), extraInfo);
         }
         return result;
     }
@@ -41,9 +97,86 @@ namespace OHOS {
     {
         bool result = false;
         if (size > 0) {
-            uint32_t testId = reinterpret_cast<uint32_t>(data);
+            uint32_t testId = static_cast<uint32_t>(atoi(data));
             std::vector<std::string> outValue;
             result = rm->GetStringArrayById(testId, outValue);
+        }
+        return result;
+    }
+
+    bool GetStringArrayByNameFuzzTest(const char* data, size_t size, ResourceManager *rm)
+    {
+        bool result = false;
+        if (size > 0) {
+            std::string testName(data, size);
+            std::vector<std::string> outValue;
+            result = rm->GetStringArrayByName(testName.c_str(), outValue);
+        }
+        return result;
+    }
+
+    bool GetPatternByIdFuzzTest(const char* data, size_t size, ResourceManager *rm)
+    {
+        bool result = false;
+        if (size > 0) {
+            uint32_t testId = static_cast<uint32_t>(atoi(data));
+            std::map<std::string, std::string> outValue;
+            result = rm->GetPatternById(testId, outValue);
+        }
+        return result;
+    }
+
+    bool GetPatternByNameFuzzTest(const char* data, size_t size, ResourceManager *rm)
+    {
+        bool result = false;
+        if (size > 0) {
+            std::string testName(data, size);
+            std::map<std::string, std::string> outValue;
+            result = rm->GetPatternByName(testName.c_str(), outValue);
+        }
+        return result;
+    }
+
+    bool GetPluralStringByIdFuzzTest(const char* data, size_t size, ResourceManager *rm)
+    {
+        bool result = false;
+        if (size > 0) {
+            uint32_t testId = static_cast<uint32_t>(atoi(data));
+            std::string outValue = "";
+            result = rm->GetPluralStringById(testId, SINGULAR_NUM, outValue);
+        }
+        return result;
+    }
+
+    bool GetPluralStringByNameFuzzTest(const char* data, size_t size, ResourceManager *rm)
+    {
+        bool result = false;
+        if (size > 0) {
+            std::string testName(data, size);
+            std::string outValue = "";
+            result = rm->GetPluralStringByName(testName.c_str(), PLURAL_NUM, outValue);
+        }
+        return result;
+    }
+
+    bool GetThemeByIdFuzzTest(const char* data, size_t size, ResourceManager *rm)
+    {
+        bool result = false;
+        if (size > 0) {
+            uint32_t testId = static_cast<uint32_t>(atoi(data));
+            std::map<std::string, std::string> outValue;
+            result = rm->GetThemeById(testId, outValue);
+        }
+        return result;
+    }
+
+    bool GetThemeByNameFuzzTest(const char* data, size_t size, ResourceManager *rm)
+    {
+        bool result = false;
+        if (size > 0) {
+            std::string testName(data, size);
+            std::map<std::string, std::string> outValue;
+            result = rm->GetThemeByName(testName.c_str(), outValue);
         }
         return result;
     }
@@ -52,7 +185,7 @@ namespace OHOS {
     {
         bool result = false;
         if (size > 0) {
-            uint32_t testId = reinterpret_cast<uint32_t>(data);
+            uint32_t testId = static_cast<uint32_t>(atoi(data));
             int outValue;
             result = rm->GetIntegerById(testId, outValue);
         }
@@ -63,9 +196,9 @@ namespace OHOS {
     {
         bool result = false;
         if (size > 0) {
-            std::string testName(reinterpret_cast<const char*>(data), size);
+            std::string testName(data, size);
             int outValue;
-            result = rm->GetIntegerByName(testName, outValue);
+            result = rm->GetIntegerByName(testName.c_str(), outValue);
         }
         return result;
     }
@@ -74,9 +207,20 @@ namespace OHOS {
     {
         bool result = false;
         if (size > 0) {
-            uint32_t testId = reinterpret_cast<uint32_t>(data);
+            uint32_t testId = static_cast<uint32_t>(atoi(data));
             bool outValue;
             result = rm->GetBooleanById(testId, outValue);
+        }
+        return result;
+    }
+
+    bool GetBooleanByNameFuzzTest(const char* data, size_t size, ResourceManager *rm)
+    {
+        bool result = false;
+        if (size > 0) {
+            std::string testName(data, size);
+            bool outValue = false;
+            result = rm->GetBooleanByName(testName.c_str(), outValue);
         }
         return result;
     }
@@ -85,9 +229,42 @@ namespace OHOS {
     {
         bool result = false;
         if (size > 0) {
-            uint32_t testId = reinterpret_cast<uint32_t>(data);
+            uint32_t testId = static_cast<uint32_t>(atoi(data));
             float outValue;
             result = rm->GetFloatById(testId, outValue);
+        }
+        return result;
+    }
+
+    bool GetFloatByNameFuzzTest(const char* data, size_t size, ResourceManager *rm)
+    {
+        bool result = false;
+        if (size > 0) {
+            std::string testName(data, size);
+            float outValue = 0.0f;
+            result = rm->GetFloatByName(testName.c_str(), outValue);
+        }
+        return result;
+    }
+
+    bool GetIntArrayByIdFuzzTest(const char* data, size_t size, ResourceManager *rm)
+    {
+        bool result = false;
+        if (size > 0) {
+            uint32_t testId = static_cast<uint32_t>(atoi(data));
+            std::vector<int> outValue;
+            result = rm->GetIntArrayById(testId, outValue);
+        }
+        return result;
+    }
+
+    bool GetIntArrayByNameFuzzTest(const char* data, size_t size, ResourceManager *rm)
+    {
+        bool result = false;
+        if (size > 0) {
+            std::string testName(data, size);
+            std::vector<int> outValue;
+            result = rm->GetIntArrayByName(testName.c_str(), outValue);
         }
         return result;
     }
@@ -96,7 +273,7 @@ namespace OHOS {
     {
         bool result = false;
         if (size > 0) {
-            uint32_t testId = reinterpret_cast<uint32_t>(data);
+            uint32_t testId = static_cast<uint32_t>(atoi(data));
             std::string outValue = "";
             result = rm->GetMediaById(testId, outValue);
         }
@@ -107,9 +284,189 @@ namespace OHOS {
     {
         bool result = false;
         if (size > 0) {
-            std::string testName(reinterpret_cast<const char*>(data), size);
+            std::string testName(data, size);
             std::string outValue = "";
-            result = rm->GetMediaByName(testName, outValue);
+            result = rm->GetMediaByName(testName.c_str(), outValue);
+        }
+        return result;
+    }
+
+    bool GetRawFilePathByNameFuzzTest(const char* data, size_t size, ResourceManager *rm)
+    {
+        bool result = false;
+        if (size > 0) {
+            std::string testName(data, size);
+            std::string outValue = "";
+            result = rm->GetRawFilePathByName(testName.c_str(), outValue);
+        }
+        return result;
+    }
+
+    bool GetRawFileDescriptorFuzzTest(const char* data, size_t size, ResourceManager *rm)
+    {
+        bool result = false;
+        if (size > 0) {
+            std::string testName(data, size);
+            ResourceManager::RawFileDescriptor outValue;
+            result = rm->GetRawFileDescriptor(testName.c_str(), outValue);
+        }
+        return result;
+    }
+
+    bool GetRawFdNdkFromHapFuzzTest(const char* data, size_t size, ResourceManager *rm)
+    {
+        bool result = false;
+        if (size > 0) {
+            std::string testName(data, size);
+            ResourceManager::RawFileDescriptor outValue;
+            result = rm->GetRawFdNdkFromHap(testName.c_str(), outValue);
+        }
+        return result;
+    }
+
+    bool CloseRawFileDescriptorFuzzTest(const char* data, size_t size, ResourceManager *rm)
+    {
+        bool result = false;
+        if (size > 0) {
+            std::string testName(data, size);
+            result = rm->CloseRawFileDescriptor(testName.c_str());
+        }
+        return result;
+    }
+
+    bool GetMediaDataByIdFuzzTest(const char* data, size_t size, ResourceManager *rm)
+    {
+        bool result = false;
+        if (size > 0) {
+            uint32_t testId = static_cast<uint32_t>(atoi(data));
+            size_t len = 0;
+            std::unique_ptr<uint8_t[]> outValue;
+            result = rm->GetMediaDataById(testId, len, outValue);
+        }
+        return result;
+    }
+
+    bool GetMediaDataByNameFuzzTest(const char* data, size_t size, ResourceManager *rm)
+    {
+        bool result = false;
+        if (size > 0) {
+            std::string testName(data, size);
+            size_t len = 0;
+            std::unique_ptr<uint8_t[]> outValue;
+            result = rm->GetMediaDataByName(testName.c_str(), len, outValue);
+        }
+        return result;
+    }
+
+    bool GetMediaBase64DataByIdFuzzTest(const char* data, size_t size, ResourceManager *rm)
+    {
+        bool result = false;
+        if (size > 0) {
+            uint32_t testId = static_cast<uint32_t>(atoi(data));
+            std::string outValue;
+            result = rm->GetMediaBase64DataById(testId, outValue);
+        }
+        return result;
+    }
+
+    bool GetMediaBase64DataByNameFuzzTest(const char* data, size_t size, ResourceManager *rm)
+    {
+        bool result = false;
+        if (size > 0) {
+            std::string testName(data, size);
+            std::string outValue;
+            result = rm->GetMediaBase64DataByName(testName.c_str(), outValue);
+        }
+        return result;
+    }
+
+    bool GetProfileDataByIdFuzzTest(const char* data, size_t size, ResourceManager *rm)
+    {
+        bool result = false;
+        if (size > 0) {
+            uint32_t testId = static_cast<uint32_t>(atoi(data));
+            size_t len = 0;
+            std::unique_ptr<uint8_t[]> outValue;
+            result = rm->GetProfileDataById(testId, len, outValue);
+        }
+        return result;
+    }
+
+    bool GetProfileDataByNameFuzzTest(const char* data, size_t size, ResourceManager *rm)
+    {
+        bool result = false;
+        if (size > 0) {
+            std::string testName(data, size);
+            size_t len = 0;
+            std::unique_ptr<uint8_t[]> outValue;
+            result = rm->GetProfileDataByName(testName.c_str(), len, outValue);
+        }
+        return result;
+    }
+
+    bool GetRawFileFromHapFuzzTest(const char* data, size_t size, ResourceManager *rm)
+    {
+        bool result = false;
+        if (size > 0) {
+            std::string testName(data, size);
+            size_t len = 0;
+            std::unique_ptr<uint8_t[]> outValue;
+            result = rm->GetRawFileFromHap(testName.c_str(), len, outValue);
+        }
+        return result;
+    }
+
+    bool GetRawFileDescriptorFromHapFuzzTest(const char* data, size_t size, ResourceManager *rm)
+    {
+        bool result = false;
+        if (size > 0) {
+            std::string testName(data, size);
+            ResourceManager::RawFileDescriptor outValue;
+            result = rm->GetRawFileDescriptorFromHap(testName.c_str(), outValue);
+        }
+        return result;
+    }
+
+    bool IsLoadHapFuzzTest(const char* data, size_t size, ResourceManager *rm)
+    {
+        bool result = false;
+        if (size > 0) {
+            std::string testName(data, size);
+            result = rm->IsLoadHap(testName);
+        }
+        return result;
+    }
+
+    bool GetRawFileListFuzzTest(const char* data, size_t size, ResourceManager *rm)
+    {
+        bool result = false;
+        if (size > 0) {
+            std::string testName(data, size);
+            std::vector<std::string> outValue;
+            result = rm->GetRawFileList(testName.c_str(), outValue);
+        }
+        return result;
+    }
+
+    bool GetResIdFuzzTest(const char* data, size_t size, ResourceManager *rm)
+    {
+        bool result = false;
+        if (size > 0) {
+            uint32_t testId = static_cast<uint32_t>(atoi(data));
+            std::string testName(data, size);
+            result = rm->GetResId(testName.c_str(), testId);
+        }
+        return result;
+    }
+
+    bool GetThemeIconsFuzzTest(const char* data, size_t size, ResourceManager *rm)
+    {
+        bool result = false;
+        if (size > 0) {
+            uint32_t testId = static_cast<uint32_t>(atoi(data));
+            std::pair<std::unique_ptr<uint8_t[]>, size_t> foregroundInfo;
+            std::pair<std::unique_ptr<uint8_t[]>, size_t> backgroundInfo;
+            result = rm->GetThemeIcons(testId, foregroundInfo, backgroundInfo);
         }
         return result;
     }
@@ -118,7 +475,7 @@ namespace OHOS {
     {
         bool result = false;
         if (size > 0) {
-            uint32_t testId = reinterpret_cast<uint32_t>(data);
+            uint32_t testId = static_cast<uint32_t>(atoi(data));
             std::string outValue = "";
             result = rm->GetStringFormatById(outValue, testId);
         }
@@ -129,11 +486,94 @@ namespace OHOS {
     {
         bool result = false;
         if (size > 0) {
-            uint32_t testId = reinterpret_cast<uint32_t>(data);
+            uint32_t testId = static_cast<uint32_t>(atoi(data));
             uint32_t outValue = 0;
             result = rm->GetColorById(testId, outValue);
         }
         return result;
+    }
+
+    bool GetColorByNameFuzzTest(const char* data, size_t size, ResourceManager *rm)
+    {
+        bool result = false;
+        if (size > 0) {
+            std::string testName(data, size);
+            uint32_t outValue = 0;
+            result = rm->GetColorByName(testName.c_str(), outValue);
+        }
+        return result;
+    }
+
+    bool GetSymbolByIdFuzzTest(const char* data, size_t size, ResourceManager *rm)
+    {
+        bool result = false;
+        if (size > 0) {
+            uint32_t testId = static_cast<uint32_t>(atoi(data));
+            uint32_t outValue = 0;
+            result = rm->GetSymbolById(testId, outValue);
+        }
+        return result;
+    }
+
+    bool GetSymbolByNameFuzzTest(const char* data, size_t size, ResourceManager *rm)
+    {
+        bool result = false;
+        if (size > 0) {
+            std::string testName(data, size);
+            uint32_t outValue = 0;
+            result = rm->GetSymbolByName(testName.c_str(), outValue);
+        }
+        return result;
+    }
+
+    bool GetProfileByIdFuzzTest(const char* data, size_t size, ResourceManager *rm)
+    {
+        bool result = false;
+        if (size > 0) {
+            uint32_t testId = static_cast<uint32_t>(atoi(data));
+            std::string outValue;
+            result = rm->GetProfileById(testId, outValue);
+        }
+        return result;
+    }
+
+    bool GetProfileByNameFuzzTest(const char* data, size_t size, ResourceManager *rm)
+    {
+        bool result = false;
+        if (size > 0) {
+            std::string testName(data, size);
+            std::string outValue;
+            result = rm->GetProfileByName(testName.c_str(), outValue);
+        }
+        return result;
+    }
+
+    void ResourceManagerImplAdd(const char* data, size_t size, ResourceManager *rm)
+    {
+        AddResourceFuzzTest(data, size, rm);
+        RemoveResourceFuzzTest(data, size, rm);
+        AddAppOverlayFuzzTest(data, size, rm);
+        RemoveAppOverlayFuzzTest(data, size, rm);
+        GetStringByNameFuzzTest(data, size, rm);
+        GetStringArrayByNameFuzzTest(data, size, rm);
+        GetPatternByIdFuzzTest(data, size, rm);
+        GetPatternByNameFuzzTest(data, size, rm);
+        GetPluralStringByIdFuzzTest(data, size, rm);
+        GetPluralStringByNameFuzzTest(data, size, rm);
+        GetThemeByIdFuzzTest(data, size, rm);
+        GetThemeByNameFuzzTest(data, size, rm);
+        GetBooleanByNameFuzzTest(data, size, rm);
+        GetFloatByNameFuzzTest(data, size, rm);
+        GetIntArrayByIdFuzzTest(data, size, rm);
+        GetIntArrayByNameFuzzTest(data, size, rm);
+        GetRawFilePathByNameFuzzTest(data, size, rm);
+        GetRawFileDescriptorFuzzTest(data, size, rm);
+        GetRawFdNdkFromHapFuzzTest(data, size, rm);
+        CloseRawFileDescriptorFuzzTest(data, size, rm);
+        GetMediaDataByIdFuzzTest(data, size, rm);
+        GetMediaDataByNameFuzzTest(data, size, rm);
+        GetMediaBase64DataByIdFuzzTest(data, size, rm);
+        GetMediaBase64DataByNameFuzzTest(data, size, rm);
     }
 
     void ResourceManagerImplFuzzTest(const char* data, size_t size)
@@ -142,6 +582,20 @@ namespace OHOS {
         if (rm == nullptr) {
             return;
         }
+        ResourceManagerImplAdd(data, size, rm);
+        GetProfileDataByIdFuzzTest(data, size, rm);
+        GetProfileDataByNameFuzzTest(data, size, rm);
+        GetRawFileFromHapFuzzTest(data, size, rm);
+        GetRawFileDescriptorFromHapFuzzTest(data, size, rm);
+        IsLoadHapFuzzTest(data, size, rm);
+        GetRawFileListFuzzTest(data, size, rm);
+        GetResIdFuzzTest(data, size, rm);
+        GetThemeIconsFuzzTest(data, size, rm);
+        GetColorByNameFuzzTest(data, size, rm);
+        GetSymbolByIdFuzzTest(data, size, rm);
+        GetSymbolByNameFuzzTest(data, size, rm);
+        GetProfileByIdFuzzTest(data, size, rm);
+        GetProfileByNameFuzzTest(data, size, rm);
         GetStringByIdFuzzTest(data, size, rm);
         GetStringArrayByIdFuzzTest(data, size, rm);
         GetIntegerByIdFuzzTest(data, size, rm);
