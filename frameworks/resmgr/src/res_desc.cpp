72308d84db5638cd6a846d4d6eddaefc283516f5
冉召宇 <ranzhaoyu1@huawei.com>
2024-11-26 01:24:15 +0000

    update frameworks/resmgr/src/res_desc.cpp.
    Signed-off-by: 冉召宇 <ranzhaoyu1@huawei.com>

--- frameworks/resmgr/src/res_desc.cpp
+++ frameworks/resmgr/src/res_desc.cpp
@@ -149,7 +149,7 @@ const std::string KeyParam::ConvertToStr() const
         char tmp[4];
         char tmp2[5];
         errno_t eret = memcpy_s(tmp, sizeof(tmp), &value_, sizeof(value_));
-        if (eret != EOK) {
+        if (eret != OK) {
             RESMGR_HILOGE(RESMGR_TAG, "memcpy_s error : %d", eret);
         }
         int j = 0;
