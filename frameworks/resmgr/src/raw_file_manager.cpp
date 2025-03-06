7f86147dbea06cebace4f8f7099ed076283a382d
冉召宇 <ranzhaoyu1@huawei.com>
2025-03-06 03:52:10 +0000

    update frameworks/resmgr/src/raw_file_manager.cpp.
    Signed-off-by: 冉召宇 <ranzhaoyu1@huawei.com>

--- frameworks/resmgr/src/raw_file_manager.cpp
+++ frameworks/resmgr/src/raw_file_manager.cpp
@@ -592,7 +592,7 @@ int64_t OH_ResourceManager_GetRawFileOffset64(const RawFile64 *rawFile)
 static bool GetRawFileDescriptorFromHap64(const RawFile64 *rawFile, RawFileDescriptor64 *descriptor)
 {
     ResourceManager::RawFileDescriptor resMgrDescriptor;
-    if (rawFile->resMgr->resManager == nullptr) {
+    if (rawFile->raw->resMgr->resManager == nullptr) {
         return false;
     }
     RState state = rawFile->raw->resMgr->resManager->
