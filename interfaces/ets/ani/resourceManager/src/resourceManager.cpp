fe5cbab44f1bd75fa18bbcabb53f29b8ba6dd26a
sunjie <sunjie69@huawei.com>
2025-06-09 14:37:28 +0800

    fix nullptr
    Signed-off-by: sunjie <sunjie69@huawei.com>
    Change-Id: I4934e7d2f144f5e44c95cdae8d94161cb61048f0

--- interfaces/ets/ani/resourceManager/src/resourceManager.cpp
+++ interfaces/ets/ani/resourceManager/src/resourceManager.cpp
@@ -615,7 +615,7 @@ ani_string ResMgrAddon::GetIntPluralStringValueSyncById(ani_env* env, ani_object
     std::shared_ptr<ResourceManager> resMgr = nullptr;
     uint32_t notUse = 0;
     bool ret = GetHapResourceManager(dataContext.get(), resMgr, notUse);
-    if (!ret) {
+    if (!ret || resMgr == nullptr) {
         RESMGR_HILOGE(RESMGR_ANI_TAG, "Failed to get resMgr in getIntPluralStringValueSyncById");
         ResourceManagerAniUtils::AniThrow(env, ERROR_CODE_RES_NOT_FOUND_BY_ID);
         return nullptr;
@@ -658,7 +658,7 @@ ani_string ResMgrAddon::GetIntPluralStringValueSync(ani_env* env, ani_object obj
     std::shared_ptr<ResourceManager> resMgr = nullptr;
     uint32_t resId = 0;
     bool ret = GetHapResourceManager(dataContext.get(), resMgr, resId);
-    if (!ret) {
+    if (!ret || resMgr == nullptr) {
         RESMGR_HILOGE(RESMGR_ANI_TAG, "Failed to get resMgr in getIntPluralStringValueSync");
         ResourceManagerAniUtils::AniThrow(env, ERROR_CODE_RES_NOT_FOUND_BY_ID);
         return nullptr;
@@ -727,7 +727,7 @@ ani_string ResMgrAddon::GetDoublePluralStringValueSyncById(ani_env* env, ani_obj
     std::shared_ptr<ResourceManager> resMgr = nullptr;
     uint32_t notUse = 0;
     bool ret = GetHapResourceManager(dataContext.get(), resMgr, notUse);
-    if (!ret) {
+    if (!ret || resMgr == nullptr) {
         RESMGR_HILOGE(RESMGR_ANI_TAG, "Failed to get resMgr in getIntPluralStringValueSyncById");
         ResourceManagerAniUtils::AniThrow(env, ERROR_CODE_RES_NOT_FOUND_BY_ID);
         return nullptr;
@@ -764,7 +764,7 @@ ani_string ResMgrAddon::GetDoublePluralStringValueSync(ani_env* env, ani_object 
     std::shared_ptr<ResourceManager> resMgr = nullptr;
     uint32_t resId = 0;
     bool ret = GetHapResourceManager(dataContext.get(), resMgr, resId);
-    if (!ret) {
+    if (!ret || resMgr == nullptr) {
         RESMGR_HILOGE(RESMGR_ANI_TAG, "Failed to get resMgr in getIntPluralStringValueSync");
         ResourceManagerAniUtils::AniThrow(env, ERROR_CODE_RES_NOT_FOUND_BY_ID);
         return nullptr;
