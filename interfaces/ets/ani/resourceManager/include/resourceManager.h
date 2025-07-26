99e28737ebc15212d9929c12412e7dd5d9b83c90
sunjie <sunjie69@huawei.com>
2025-07-26 14:08:09 +0800

    回合0603到master
    Signed-off-by: sunjie <sunjie69@huawei.com>
    Change-Id: I51530cbcc87d305cc6b5aacbc0981a9f30d3bfb0

--- interfaces/ets/ani/resourceManager/include/resourceManager.h
+++ interfaces/ets/ani/resourceManager/include/resourceManager.h
@@ -31,33 +31,74 @@ public:
     ResMgrAddon(const std::string& bundleName, const std::shared_ptr<ResourceManager>& resMgr,
         const std::shared_ptr<OHOS::AbilityRuntime::Context>& context, bool isSystem = false);
     ResMgrAddon(const std::shared_ptr<ResourceManager>& resMgr, bool isSystem = false);
+    ani_object CreateOverrideAddon(ani_env* env, const std::shared_ptr<ResourceManager>& resMgr);
+
     static ani_object CreateResMgr(ani_env* env, const std::string& bundleName,
         const std::shared_ptr<ResourceManager>& resMgr, std::shared_ptr<AbilityRuntime::Context> context);
     static ani_object GetSystemResourceManager(ani_env* env);
 
     static ani_string GetStringSyncById(ani_env* env, ani_object object, ani_double resId);
     static ani_string GetFormatStringSyncById(ani_env *env, ani_object object, ani_double resId, ani_object args);
-    static ani_string GetStringSync(ani_env* env, ani_object object, ani_object resource);
-    static ani_string GetFormatStringSync(ani_env *env, ani_object object, ani_object resource, ani_object args);
+    
+    static ani_string GetStringByNameSync(ani_env* env, ani_object object, ani_string resName);
+    static ani_string GetFormatStringByNameSync(ani_env *env, ani_object object, ani_string resName, ani_object args);
+
+    static ani_boolean GetBooleanById(ani_env* env, ani_object object, ani_double resId);
+    static ani_boolean GetBooleanByName(ani_env* env, ani_object object, ani_string resName);
+
     static ani_double GetNumberById(ani_env* env, ani_object object, ani_double resId);
-    static ani_double GetNumber(ani_env* env, ani_object object, ani_object resource);
+    static ani_double GetNumberByName(ani_env* env, ani_object object, ani_string resName);
+
     static ani_double GetColorSyncById(ani_env* env, ani_object object, ani_double resId);
-    static ani_double GetColorSync(ani_env* env, ani_object object, ani_object resource);
-    static ani_object GetRawFileContentSync(ani_env* env, ani_object object, ani_string path);
 
     static ani_string GetIntPluralStringValueSyncById(ani_env* env, ani_object object,
         ani_double resId, ani_double num, ani_object args);
-    static ani_string GetIntPluralStringValueSync(ani_env* env, ani_object object,
-        ani_object resource, ani_double num, ani_object args);
+
     static ani_string GetIntPluralStringByNameSync(ani_env* env, ani_object object,
         ani_string resName, ani_double num, ani_object args);
 
     static ani_string GetDoublePluralStringValueSyncById(ani_env* env, ani_object object,
         ani_double resId, ani_double num, ani_object args);
-    static ani_string GetDoublePluralStringValueSync(ani_env* env, ani_object object,
-        ani_object resource, ani_double num, ani_object args);
+
     static ani_string GetDoublePluralStringByNameSync(ani_env* env, ani_object object,
         ani_string resName, ani_double num, ani_object args);
+
+    static ani_double GetColorByNameSync(ani_env* env, ani_object object, ani_string resName);
+
+    static void AddResource(ani_env* env, ani_object object, ani_string path);
+    static void RemoveResource(ani_env* env, ani_object object, ani_string path);
+
+    static ani_object GetRawFdSync(ani_env* env, ani_object object, ani_string path);
+    static void CloseRawFdSync(ani_env* env, ani_object object, ani_string path);
+    static ani_boolean IsRawDir(ani_env* env, ani_object object, ani_string path);
+
+    static ani_object GetRawFileListSync(ani_env* env, ani_object object, ani_string path);
+    static ani_object GetRawFileContentSync(ani_env* env, ani_object object, ani_string path);
+
+    static ani_object GetMediaContentSyncById(ani_env* env, ani_object object,
+        ani_double resId, ani_object density);
+
+
+    static ani_string GetMediaContentBase64SyncById(ani_env* env, ani_object object,
+        ani_double resId, ani_object density);
+
+
+    static ani_object GetStringArrayValueSyncById(ani_env* env, ani_object object, ani_double resId);
+    static ani_object GetStringArrayByNameSync(ani_env* env, ani_object object, ani_string resName);
+
+    static ani_object GetMediaByNameSync(ani_env* env, ani_object object, ani_string resName, ani_object density);
+    static ani_string GetMediaBase64ByNameSync(ani_env* env, ani_object object, ani_string resName, ani_object density);
+
+    static ani_object GetConfigurationSync(ani_env* env, ani_object object);
+    static ani_object GetDeviceCapabilitySync(ani_env* env, ani_object object);
+    static ani_object GetLocales(ani_env* env, ani_object object, ani_object includeSystem);
+
+    static ani_double GetSymbolById(ani_env* env, ani_object object, ani_double resId);
+    static ani_double GetSymbolByName(ani_env* env, ani_object object, ani_string resName);
+
+    static ani_object GetOverrideResourceManager(ani_env* env, ani_object object, ani_object configuration);
+    static ani_object GetOverrideConfiguration(ani_env* env, ani_object object);
+    static void UpdateOverrideConfiguration(ani_env* env, ani_object object, ani_object configuration);
     static ani_status BindContext(ani_env* env);
 
     inline std::shared_ptr<ResourceManager> GetResMgr()
