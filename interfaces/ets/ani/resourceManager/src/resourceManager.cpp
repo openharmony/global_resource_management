bc2f49578bf3343f4289c140da99bffc2252c660
sj <sunjie69@h-partners.com>
2025-12-08 15:45:03 +0800

    缓存resourcemanager类对象,优化性能
    Signed-off-by: sj <sunjie69@h-partners.com>

--- interfaces/ets/ani/resourceManager/src/resourceManager.cpp
+++ interfaces/ets/ani/resourceManager/src/resourceManager.cpp
@@ -15,6 +15,7 @@
 
 #include "resourceManager.h"
 
+#include <mutex>
 #include "ani_signature.h"
 #include "hilog_wrapper.h"
 #include "resource_manager.h"
@@ -22,7 +23,10 @@
 namespace OHOS {
 namespace Global {
 namespace Resource {
+static ani_ref g_clsRef = nullptr;
+static ani_method g_ctor = nullptr;
 
+static std::mutex g_initMutex;
 ani_object ResMgrAddon::CreateResMgr(
     ani_env* env, const std::string& bundleName, const std::shared_ptr<ResourceManager>& resMgr,
     std::shared_ptr<AbilityRuntime::Context> context)
@@ -32,26 +36,42 @@ ani_object ResMgrAddon::CreateResMgr(
 
 ani_object ResMgrAddon::WrapResourceManager(ani_env* env, std::shared_ptr<ResourceManager> resMgr)
 {
+    if (!Init(env)) {
+        return nullptr;
+    }
     ani_object nativeResMgr;
-    ani_class cls;
-    if (ANI_OK != env->FindClass(AniSignature::RESOURCE_MANAGER_INNER, &cls)) {
-        RESMGR_HILOGE(RESMGR_ANI_TAG, "Find class '%{public}s' failed", AniSignature::RESOURCE_MANAGER_INNER);
-        return nullptr;
-    }
-
-    ani_method ctor;
-    if (ANI_OK != env->Class_FindMethod(cls, "<ctor>", nullptr, &ctor)) {
-        RESMGR_HILOGE(RESMGR_ANI_TAG, "Find method '<ctor>' failed");
-        return nullptr;
-    }
-
     auto resMgrPtr = std::make_unique<std::shared_ptr<ResourceManager>>(resMgr);
-    if (ANI_OK != env->Object_New(cls, ctor, &nativeResMgr, reinterpret_cast<ani_long>(resMgrPtr.get()))) {
+    if (ANI_OK != env->Object_New(static_cast<ani_class>(g_clsRef), g_ctor, &nativeResMgr, reinterpret_cast<ani_long>(resMgrPtr.get()))) {
         RESMGR_HILOGE(RESMGR_ANI_TAG, "New object '%{public}s' failed", AniSignature::RESOURCE_MANAGER_INNER);
         return nullptr;
     }
     resMgrPtr.release();
     return nativeResMgr;
+}
+
+bool ResMgrAddon::Init(ani_env* env)
+{
+    std::lock_guard<std::mutex> lock(g_initMutex);
+    if (g_ctor && g_clsRef) {
+        return true;
+    }
+    ani_class cls;
+    if (ANI_OK != env->FindClass(AniSignature::RESOURCE_MANAGER_INNER, &cls)) {
+        RESMGR_HILOGE(RESMGR_ANI_TAG, "Find class '%{public}s' failed", AniSignature::RESOURCE_MANAGER_INNER);
+        return false;
+    }
+
+    if (ANI_OK != env->Class_FindMethod(cls, "<ctor>", nullptr, &g_ctor)) {
+        RESMGR_HILOGE(RESMGR_ANI_TAG, "Find method '<ctor>' failed");
+        g_ctor = nullptr;
+        return false;
+    }
+
+    if (ANI_OK != env->GlobalReference_Create(static_cast<ani_ref>(cls), &g_clsRef)) {
+        RESMGR_HILOGE(RESMGR_ANI_TAG, "Create global resourceManager ref failed");
+        return false;
+    }
+    return true;
 }
 } // namespace Resource
 } // namespace Global
