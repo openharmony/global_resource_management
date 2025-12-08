b3a162f7d7387d51d1bb20086356e100a9a19bca
jckon <sunjie69@h-partners.com>
2025-12-08 18:04:24 +0800

    update: 更新文件 resourceManager.cpp
    Signed-off-by: jckon <sunjie69@h-partners.com>

--- interfaces/ets/ani/resourceManager/src/resourceManager.cpp
+++ interfaces/ets/ani/resourceManager/src/resourceManager.cpp
@@ -24,7 +24,9 @@ namespace OHOS {
 namespace Global {
 namespace Resource {
 static ani_class g_cls = nullptr;
+static ani_ref g_cls_ref = nullptr;
 static ani_method g_ctor = nullptr;
+
 static std::mutex g_initMutex;
 ani_object ResMgrAddon::CreateResMgr(
     ani_env* env, const std::string& bundleName, const std::shared_ptr<ResourceManager>& resMgr,
@@ -51,19 +53,23 @@ ani_object ResMgrAddon::WrapResourceManager(ani_env* env, std::shared_ptr<Resour
 bool ResMgrAddon::Init(ani_env* env)
 {
     std::lock_guard<std::mutex> lock(g_initMutex);
-    if (g_cls && g_ctor) {
+    if (g_cls && g_ctor && g_cls_ref) {
         return true;
     }
-    
+
     if (ANI_OK != env->FindClass(AniSignature::RESOURCE_MANAGER_INNER, &g_cls)) {
         RESMGR_HILOGE(RESMGR_ANI_TAG, "Find class '%{public}s' failed", AniSignature::RESOURCE_MANAGER_INNER);
-        g_cls = nullptr;
         return false;
     }
 
     if (ANI_OK != env->Class_FindMethod(g_cls, "<ctor>", nullptr, &g_ctor)) {
         RESMGR_HILOGE(RESMGR_ANI_TAG, "Find method '<ctor>' failed");
         g_ctor = nullptr;
+        return false;
+    }
+
+    if (ANI_OK != env->GlobalReference_Create(static_cast<ani_ref>(g_cls), &g_cls_ref)) {
+        RESMGR_HILOGE(RESMGR_ANI_TAG, "Create global resourceManager ref failed");
         return false;
     }
     return true;
